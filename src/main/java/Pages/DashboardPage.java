0c9388abaa5a3bae287047fa8e69a83e1da7ab4a
Abhishek Sinha <abhishek.mcsd@gmail.com>
2019-10-07 21:12:34 +0200

    Removed unwanted code

--- src/main/java/Pages/DashboardPage.java
+++ src/main/java/Pages/DashboardPage.java
@@ -8,15 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 import Base.BasePage;
 
 public class DashboardPage extends BasePage {
-	@FindBy(xpath="//a[@id='welcome']")
-	private WebElement WelcomeAdmin;
-	
-	@FindBy(xpath="//a[text()='Logout']")
-	private WebElement LogoutButton;
-	
-	@FindBy(xpath="//b[text()='PIM']")
-	private WebElement PIM;
-	
+		
 	@FindBy(xpath="//input[@name='tripType' and @value='roundtrip']")
 	private WebElement FlightType;
 	
@@ -41,11 +33,6 @@ public class DashboardPage extends BasePage {
 	@FindBy(xpath="//select[@name='toDay']")
 	private WebElement ReturningDay;
 	
-	
-	
-	
-	
-	
 	public DashboardPage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
@@ -54,19 +41,4 @@ public class DashboardPage extends BasePage {
 	public String getDashBoardPageTitle() {
 		return driver.getTitle();
 	}
-	
-	public void PIMClick() {
-		
-		PIM.click();
-	}
-	
-	
-	
-	public void logout() {
-		WelcomeAdmin.click();
-		LogoutButton.click();
-		
-	}
-
-
 }
